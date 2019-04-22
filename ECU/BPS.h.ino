7f1637390b9088bef0f1cb6f3323de3f5bf26c35
Arslan Aziz <aaa2tj@virginia.edu>
2019-04-22 19:38:17 -0400

    first commit of BPS code (unfinished)

---
+++ ECU/BPS.h.ino
@@ -0,0 +1,85 @@
+/*
+ * Arslan Aziz
+ * Interface with Orion standard BPS v1 over CAN
+ * Updates battery & bps related fields
+ * Uses pawelsky fork of FlexCan library found at
+ * https://github.com/pawelsky/FlexCAN_Library
+ */
+
+#ifndef bps_h
+#define bps_h
+
+#include <math.h>
+#include <FlexCAN.h>
+#include <kinetis_flexcan.h>
+#include <cstdlib>
+#include <string>
+#include "pindef.h"
+
+typedef struct batt_status{
+  uint16_t pack_current;
+  uint16_t pack_voltage;
+  uint8_t pack_SOC;
+  uint8_t pack_high_temp;
+  uint8_t pack_avg_temp; 
+} batt_status;
+
+typedef struct bps_status{
+  //bools break out from 8 bit relay_status message
+  bool discharge_enable;
+  bool charge_enable;
+  bool charge_safety_enable;
+  bool malfunction;
+  bool multi_purpose_out_enable;
+  bool always_on_signal_status;
+  bool ready_signal_status;
+  bool charge_signal_status;
+  uint16_t pack_charge_limit;
+  uint16_t pack_discharge_limit;
+  //each bit of current_limit_status corresponds to reason for current limit change
+  //TODO: break out current_limit_status into bools, interpret with parse function
+  uint16_t current_limit_status;
+} bps_status;
+
+typedef struct bps_errors{
+  bool P0A1F;//internal comm fault
+  bool P0A00;//internal conversion fault
+  bool P0A80;//weak cell fault
+  bool P0AFA;//low cell volt fault
+  bool U0100;//comm fault?
+  bool P0A04;//open wiring fault
+  bool P0AC0;//current sensor fault
+  bool P0A01;//pack volt sensor fault
+  bool P0A02;//weak pack fault
+  bool P0A03;//volt redundancy fault
+  bool P0A81;//fan monitor fault
+  bool P0A9C;//thermistor fault
+  bool P0560;//always-on supply fault
+  bool P0AA6;//high volt isolation fault
+  bool P0A05;//12 volt power supply fault
+  bool P0A06;//charge limit enforemcent fault
+  bool P0A07;//discharge limit enforcement fault
+  bool P0A08;//charger safety relay fault
+  bool P0A09;//internal memory fault
+  bool P0A0A;//internal thermistor fault
+  bool P0A0B;//internal logic fault
+  
+} bps_errors;
+
+class BPS{
+
+  private:
+    uint8_t id;
+
+  public:
+    batt_status batt_stat;
+    bps_status bps_stat;
+    bps_errors errors;
+
+    BPS(uint8_t addr);
+    
+    uint8_t bps_parse(const CAN_message_t &msg);
+
+    void print();
+}
+#endif
